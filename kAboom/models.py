bec50c41d9f3d4d01022c349b37fa18d33102b3d
Artem <posevin2011@gmail.com>
2022-11-29 21:28:20 +0300

    commit 42

--- kAboom/models.py
+++ kAboom/models.py
@@ -73,6 +73,7 @@ class Playlist(models.Model):
     name = models.CharField(max_length=120)
     favorite = models.ManyToManyField(UserProfile)
     maker = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='favorite', null=True)
+    user_maker = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
 
 class Employee(models.Model):
