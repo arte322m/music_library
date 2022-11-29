7a5ff42236cae366a2145bd0892f21459be9334d
Artem <posevin2011@gmail.com>
2022-11-29 08:18:07 +0300

    commit 41.11232999

--- kAboom/management/commands/zen.py
+++ kAboom/management/commands/zen.py
@@ -1,6 +1,6 @@
-from django.core.management.base import BaseCommand
 import os.path
 import sqlite3
+from django.core.management.base import BaseCommand
 from kAboom.models import Genre, Artist, MediaType, Playlist, Album, Track, Employee, Customer, Invoice, InvoicesItem
 
 
