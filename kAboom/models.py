66de2009712a5025c5960179f42f9e16348effdb
Artem <posevin2011@gmail.com>
2022-12-20 17:14:36 +0300

    commit hz9

--- kAboom/models.py
+++ kAboom/models.py
@@ -15,8 +15,14 @@ from django.contrib.auth.models import User
 
 
 class UserProfile(models.Model):
+    DARK = 'DARK'
+    LIGHT = 'LIGHT'
+    TYPE_OF_THEME = [
+        (DARK, 'Dark'),
+        (LIGHT, 'Light'),
+    ]
     user = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
-    type_of_theme = models.BooleanField(default=False)
+    type_of_theme = models.CharField(max_length=7, choices=TYPE_OF_THEME, default=DARK)
 
 
 class MediaType(models.Model):
