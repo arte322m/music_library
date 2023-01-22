500d4a493fed364d23d58645a264a2f4a9c79fd6
Artem <posevin2011@gmail.com>
2023-01-22 09:09:58 +0300

    commit 55.1111

---
+++ kAboom/Bsoup.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get('https://muzati.net/').text
+soup = BeautifulSoup(response, 'html.parser')
+
+filtered = {}
+all_find = soup.findAll('a')
+for i in all_find:
+    if i.text == 'Новинки':
+        break
+    if i.find('div', class_='track-title'):
+        filtered[i.find('div', class_='track-title').text] = [
+            i.find('div', class_='song').text,
+            i.find('div', class_='songinfo').text,
+            i.get('href')
+        ]
+
+for i in filtered.items():
+    print(i)
