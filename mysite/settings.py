bb91bbf489e9065c55eb30da121d6cdab4572c07
Artem <posevin2011@gmail.com>
2023-01-31 17:50:24 +0300

    commit 57 add debug toolbar

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
@@ -129,3 +131,7 @@ CACHES = {
         'LOCATION': os.path.join(BASE_DIR, 'mysite_cache'),
     }
 }
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
