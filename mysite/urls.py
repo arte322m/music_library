ca1d2169887f448c88ee7bbf5e94ef16d7cc8124
Artem <posevin2011@gmail.com>
2023-02-01 15:27:24 +0300

    commit 57.11 add debug toolbar

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from mysite import settings
 from django.contrib import admin
 from django.urls import include, path
+
+from mysite import settings
 
 urlpatterns = [
     path('', include('kAboom.urls')),
