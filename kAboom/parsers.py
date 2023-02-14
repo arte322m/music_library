0306b245a7b29b32b4530c4d8c2a8a4a4b83200e
Artem <posevin2011@gmail.com>
2023-02-14 12:08:42 +0300

    commit 58.11

--- kAboom/parsers.py
+++ kAboom/parsers.py
@@ -2,6 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 
 URL_MUZATI = 'https://muzati.net/'
+URL_MP3BOB = 'https://mp3bob.ru/load/top-nedeli/'
+
+
+# class result:
+#     pass
 
 
 def check(response):
@@ -55,8 +60,50 @@ def trend_of_main_page():
     return result
 
 
+def get_info_track_2(url: str):
+    result = {}
+    response = requests.get(url)
+    check(response)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    song_details = soup.findAll('div', class_='song_detal_p')
+    tags_nav = soup.find('div', class_='path_navigation clearfix')
+    tags = tags_nav.findAll('li')
+    for tag in tags:
+        tag_text = tag.text
+        if 'Главная' in tag_text:
+            continue
+        result.setdefault('Жанры', [])
+        result['Жанры'].append(tag_text[1:])
+    for detail in song_details:
+        detail_text = detail.text
+        size = detail.find('span', class_='song_zzz').text.split(': ')[1]
+        result['Размер'] = size
+        if 'Время' in detail_text:
+            duration = detail_text.split(': ')[1]
+            result['Продолжительность'] = duration.split(' ')[0]
+
+
+    return result
+
+
+def top30_week():
+    response = requests.get(URL_MP3BOB, timeout=20)
+    check(response)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    result = {}
+    all_find = soup.findAll('div', class_='song-item')
+    for i in all_find:
+        track = i.find('span', class_='song_nazv __adv_name').text
+        artist = i.find('span', class_='song_artist __adv_artist').text
+        url = i.find('a', href=True).get('href')
+        result.setdefault(artist, {})
+        result[artist].setdefault(track, get_info_track_2(f'https://mp3bob.ru/{url}'))
+    return result
+
+
 def main():
-    trend_of_main_page()
+    print(top30_week())
+    # trend_of_main_page()
 
 
 if __name__ == '__main__':
